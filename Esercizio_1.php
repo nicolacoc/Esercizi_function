0f75e5f1482ce47bfb4afc92004e0c6b24162f1f
Nicola Cocco <niki@motoimco.it>
2024-09-27 15:49:31 +0200

    New commit

--- Esercizio_1.php
+++ Esercizio_1.php
@@ -1,8 +1,7 @@
 <?php
 function primeNumber(int $number):bool{
 if ($number >= 2){
-    $operation = ($number == 2 || $number % 2 > 0);
-    return ($operation)?true:false;
+    return ($number == 2 || $number % 2 > 0)?true:false;
     }else{
     return false;
 }
