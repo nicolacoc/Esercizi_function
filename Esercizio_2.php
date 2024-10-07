98bb5a6adcb031d33ccb2843d4b6554fde2f3110
Nicola Cocco <niki@motoimco.it>
2024-10-07 16:24:24 +0200

    New commit

--- Esercizio_2.php
+++ Esercizio_2.php
@@ -6,13 +6,12 @@ function GetPrimeNumber(int $startNumber, int $endNumber = 10): array
     require_once('Esercizio_1.php');
     $Numbers = array();
     for ($i = $startNumber, $ok = 1; $ok <= $endNumber; $i++) {
-        if ($i > 0) {
             $number = primeNumber($i);
-            if ($number) {
+            if ($i > 0 && $number) {
                 $Numbers[] = $i;
                 $ok++;
             }
-        }
+
 
     }
     return $Numbers;
